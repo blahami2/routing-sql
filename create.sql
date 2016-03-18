fce515cd37e57e8cc7e6dba736f2d3701afe3cf5
Michael Bláha <blahami2@gmail.com>
2016-03-18 14:54:46 +0100

    Added create speed_map

--- create.sql
+++ create.sql
@@ -23,4 +23,29 @@ CREATE INDEX road_types_type_id_idx
   (type_id);
 
 
+-- Table: public.speed_map
+-- DROP TABLE public.speed_map;
+
+CREATE TABLE public.speed_map
+(
+  road_type integer NOT NULL,
+  state character(2) NOT NULL,
+  speed integer,
+  CONSTRAINT speed_map_pkey PRIMARY KEY (road_type, state)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.speed_map
+  OWNER TO postgres;
+
+-- Index: public.speed_map_road_type_state_idx
+
+-- DROP INDEX public.speed_map_road_type_state_idx;
+
+CREATE INDEX speed_map_road_type_state_idx
+  ON public.speed_map
+  USING btree
+  (road_type, state COLLATE pg_catalog."default");
+
 
