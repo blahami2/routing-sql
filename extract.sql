172626b39858fe7b8d3a2a31e297f20731ce8ece
Michael Bláha <blahami2@gmail.com>
2016-03-18 14:20:48 +0100

    added notes, renamed script

---
+++ extract.sql
@@ -0,0 +1,23 @@
+﻿CREATE OR REPLACE VIEW valid_ways AS
+SELECT * FROM ways WHERE public."_isValidWay"(ways);
+
+CREATE OR REPLACE VIEW valid_nodes AS
+SELECT nodes.*--, ST_AsX3D(geom) AS geom, amount, ST_AsText(nodes.geom) 
+FROM nodes JOIN
+        (SELECT res.node_id FROM
+            (SELECT w.node_id, COUNT(*) AS amount FROM
+                (SELECT unnest(fw.nodes) AS node_id,* FROM 
+                    valid_ways AS fw 
+                ) AS w 
+            GROUP BY (w.node_id)) AS res
+            WHERE (
+                res.amount<>1 
+                AND
+                res.amount<>0
+                ) 
+        UNION
+        SELECT nodes.id AS node_id FROM
+	    nodes JOIN valid_ways AS ways
+	    ON (nodes.id = ways.nodes[array_lower(nodes,1)] OR nodes.id = ways.nodes[array_upper(nodes,1)])
+        ) AS valid
+ON nodes.id = valid.node_id;
--- notes.sql
+++ notes.sql
@@ -13,7 +13,12 @@ SELECT column_name(s)
 FROM table_name
 WHERE condition
 
+-- DIFFERENCE
+SELECT * FROM a
+EXCEPT
+SELECT * FROM b;
+
 -- PERFORMANCE
 -- extract.sql execution - 4s, COUNT 2s
 -- creating ways view - 0.067s
--- extract.sql execution using ways view - 4.1s, COUNT 2.2s
+-- extract.sql execution using ways view - 4.1s, COUNT 2.2s (variable 3s)
