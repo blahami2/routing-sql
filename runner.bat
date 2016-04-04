d54eb3e7aae7acb376bd99474310f8cf6f1a3a30
Michael Bláha <blahami2@gmail.com>
2016-04-04 13:17:45 +0200

    Added authentication and more options to the script, dbauth is no longer required

--- runner.bat
+++ runner.bat
@@ -10,19 +10,23 @@ echo Start: %time% >> log.txt
 echo Running database import: %1
 echo Start: %time%
 
-set database=osm_de
+set database=osm_prague
+set tablespace=pg_default
+set temp_tablespace=temporary_ssd
 set dbuser=postgres
-set inputpbf="C:\Routing\Data\DE.pbf"
+set dbhost=localhost
+set dbpassword=password
+set inputpbf="C:\Routing\Data\prague.pbf"
 ::WARNING!!! Do not forget to edit dbauth.txt as well!!!
 
 ::goto index
 
 :createdb
 dropdb -U %dbuser% %database%
-createdb -U %dbuser% -O %dbuser% -E utf8 -D osm --lc-collate="Czech_Czech Republic.1250" --lc-ctype="Czech_Czech Republic.1250" %database%
+createdb -U %dbuser% -O %dbuser% -E utf8 -D %tablespace% --lc-collate="Czech_Czech Republic.1250" --lc-ctype="Czech_Czech Republic.1250" %database%
 
 :tmp
-::psql -U %dbuser% -d %database% -c "ALTER DATABASE %database% SET temp_tablespaces = 'temporary_hdd';" > NUL  
+psql -U %dbuser% -d %database% -c "ALTER DATABASE %database% SET temp_tablespaces = '%temp_tablespace%';" > NUL  
 :create
 psql -U %dbuser% -d %database% -f "C:\Program Files\PostgreSQL\9.5\share\contrib\postgis-2.2\postgis.sql" > NUL
 psql -U %dbuser% -d %database% -f "C:\Program Files\PostgreSQL\9.5\share\contrib\postgis-2.2\spatial_ref_sys.sql" > NUL  
@@ -36,10 +40,10 @@ echo database creation time: %time% >> log.txt
 echo database creation time: %time%
 
 :import
-call osmosis --read-pbf %inputpbf% --log-progress --write-pgsql authFile=dbauth.txt
+call osmosis --read-pbf %inputpbf% --log-progress --write-pgsql host=%dbhost% database=%database% user=%dbuser% password=%dbpassword% dbType=postgresql
 goto osmtime 
 :update
-call osmosis --read-pbf %1 --log-progress --write-pgsql-change authFile=dbauth.txt
+call osmosis --read-pbf %1 --log-progress --write-pgsql-change host=%dbhost% database=%database% user=%dbuser% password=%dbpassword% dbType=postgresql
 :osmtime  
 echo osmosis time: %time% >> log.txt  
 echo osmosis time: %time%
