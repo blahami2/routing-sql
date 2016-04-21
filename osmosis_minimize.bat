e7a5a8a106f8c8b49fc8ae73f0ae15b1cce906ea
Michael Bláha <blahami2@gmail.com>
2016-04-21 16:21:10 +0200

    Osmosis pbf minimizer for relevant data only

---
+++ osmosis_minimize.bat
@@ -0,0 +1,15 @@
+set inputpbf=C:\Routing\Data\europe.pbf
+set outputpbf=C:\Routing\Data\europe_parsed.pbf
+set JAVACMD_OPTIONS=-server -Xmx4g
+
+::barrier_whitelist = { ["cattle_grid"] = true, ["border_control"] = true, ["checkpoint"] = true, ["toll_booth"] = true, ["sally_port"] = true, ["gate"] = true, ["lift_gate"] = true, ["no"] = true, ["entrance"] = true }
+::access_tag_whitelist = { ["yes"] = true, ["motorcar"] = true, ["motor_vehicle"] = true, ["vehicle"] = true, ["permissive"] = true, ["designated"] = true, ["destination"] = true }
+::access_tag_blacklist = { ["no"] = true, ["private"] = true, ["agricultural"] = true, ["forestry"] = true, ["emergency"] = true, ["psv"] = true, ["delivery"] = true }
+::access_tag_restricted = { ["destination"] = true, ["delivery"] = true }
+::access_tags_hierarchy = { "motorcar", "motor_vehicle", "vehicle", "access" }
+::service_tag_restricted = { ["parking_aisle"] = true }
+::restriction_exception_tags = { "motorcar", "motor_vehicle", "vehicle" }
+
+
+
+call osmosis --read-pbf %inputpbf% --tf accept-relations type=boundary,restriction --tf accept-ways highway=* access=* --tf reject-ways highway=motorway,motorway_link access=no,private,agricultural,forestry,emergency,psv,delivery --tf reject-relations --used-node idTrackerType=BitSet --write-pbf %outputpbf%
